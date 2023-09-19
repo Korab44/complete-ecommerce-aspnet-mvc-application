6837d176478c06fe6d83018c9f13f8f1ce5194f2
korab <korab.morina444@gmail.com>
2023-09-19 09:48:18 +0200

    establishing entity base repository

--- eTickets/Data/Services/Interfaces/IActorsService.cs
+++ eTickets/Data/Services/Interfaces/IActorsService.cs
@@ -1,13 +1,9 @@
-﻿using eTickets.Models;
+﻿using eTickets.Data.Base;
+using eTickets.Models;
 
 namespace eTickets.Data.Services.Interfaces
 {
-    public interface IActorsService
+    public interface IActorsService:IEntityBaseRespository<Actor>
     {
-        Task<IEnumerable<Actor>> GetAllAsync();
-        Task<Actor> GetByIdAsync(int id);
-        Task AddAsync(Actor actor);
-        Task <Actor> UpdateAsync(int id, Actor newActor);
-        Task DeleteAsync (int id);
     }
 }
